17d5bdb1f564923c3580476a2529052a9c25af36
nicolascava <cava.nicolas@gmail.com>
2018-03-25 21:40:12 -0400

    Working on tests

--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -1,26 +1,26 @@
-var Token = artifacts.require('./CavaToken.sol');
-var Crowdsale = artifacts.require('./CavaTokenCrowdsale.sol');
+const Token = artifacts.require('./CavaToken.sol');
+const Crowdsale = artifacts.require('./CavaTokenCrowdsale.sol');
 
-module.exports = function (deployer) {
+module.exports = deployer =>
   // Initial supply of 2030 because of 260 working days at a rate of 8 hours of work minus 50 days
   // of closed days ((260 * 8) - 50).
   deployer.deploy(Token, 2870, 'Cava', 'CAVA', 18).then(() => {
-    var openingTime = new Date().getTime() / 1000;
+    const openingTime = new Date().getTime() / 1000;
 
     // Ending time: 1 hour after opening.
-    var closingTime = openingTime + 3600;
+    const closingTime = openingTime + 3600;
 
     // Coinbase address.
-    var fundsAddress = '0x627306090abab3a6e1400e9345bc60c78a8bef57';
+    const fundsAddress = '0x627306090abab3a6e1400e9345bc60c78a8bef57';
 
     // Goal of 50 Ether.
-    var goal = 50000000000000000000;
+    const goal = 50000000000000000000;
 
     // We give 2000000000000000 token by Wei (~1 USD).
-    var rate = 2000000000000000;
+    const rate = 2000000000000000;
 
     // Crowdsale cap of 100 Ether.
-    var cap = 100000000000000000000;
+    const cap = 100000000000000000000;
 
     // Arguments:
     // - Minimum amount of funds to be raised (in Wei).
@@ -30,7 +30,7 @@ module.exports = function (deployer) {
     // - Address where collected funds will be forwarded to.
     // - Address of the token being sold.
     // - Crowdsale cap (in Wei).
-    deployer.deploy(
+    return deployer.deploy(
       Crowdsale,
       goal,
       openingTime,
@@ -41,4 +41,3 @@ module.exports = function (deployer) {
       cap
     );
   });
-};
--- test/cavaToken.js
+++ test/cavaToken.js
@@ -1,19 +1,87 @@
 const Token = artifacts.require('./CavaToken.sol');
 
-contract('CavaToken', () => {
-  it('should set a total supply of 2870 * 10^decimals', () => {});
-  it('should put the total supply in the contract creator balance', () => {});
-  it('should transfer tokens from one address to another', () => {});
-  it('should burn tokens', () => {});
+contract('CavaToken', (accounts) => {
+  const firstAccount = accounts[0];
+  const secondAccount = accounts[1];
+  const thirdAccount = accounts[2];
+
+  let instance = null;
+
+  beforeEach(async () => {
+    instance = await Token.deployed();
+  });
+
+  it('should set a total supply of 2870 * 10^decimals', async () => {
+    const totalSupply = await instance.totalSupply();
+
+    assert.equal(totalSupply.valueOf(), 2870 * 10 ** 18);
+  });
+
+  it('should put the total supply in the contract creator balance', async () => {
+    const totalSupply = await instance.totalSupply();
+    const ownerBalance = await instance.balanceOf(firstAccount);
+
+    assert.equal(totalSupply.valueOf(), ownerBalance.valueOf());
+  });
+
+  it('should transfer tokens from one address to another', async () => {
+    const allowedAmount = 100 * 10 ** 18;
+
+    await instance.transfer(secondAccount, allowedAmount);
+    await instance.approve(firstAccount, allowedAmount, { from: secondAccount });
+    await instance.transferFrom(secondAccount, thirdAccount, allowedAmount);
+
+    const balanceToCheck = await instance.balanceOf(thirdAccount);
+
+    assert.equal(balanceToCheck.toNumber(), allowedAmount);
+  });
+
+  it('should burn tokens', async () => {
+    const amountToBurn = 100 * 10 ** 18;
+
+    const balanceBeforeBurn = await instance.balanceOf(firstAccount);
+    const totalSupplyBeforeBurn = await instance.totalSupply();
+
+    await instance.burn(amountToBurn);
+
+    const balanceToCheck = await instance.balanceOf(firstAccount);
+
+    assert.equal(balanceToCheck.toNumber(), balanceBeforeBurn.toNumber() - amountToBurn);
+
+    const totalSupply = await instance.totalSupply();
+
+    assert.equal(totalSupply.toNumber(), totalSupplyBeforeBurn.toNumber() - amountToBurn);
+  });
+
   it('should have a name', () => {});
   it('should have a symbol', () => {});
   it('should have decimals', () => {});
-  it('should approve an address to spend a specified amount of tokens on behalf of the sender', () => {});
+
+  it('should allow an account to spend a specified amount of tokens on behalf of the sender', async () => {
+    const allowedAmount = 100 * 10 ** 18;
+
+    await instance.approve(secondAccount, allowedAmount);
+
+    const valueToCheck = await instance.allowance(firstAccount, secondAccount);
+
+    assert.equal(valueToCheck.toNumber(), allowedAmount);
+  });
+
   it('should return the amount of tokens that an owner allowed to a spender', () => {});
   it('should increase the amount of tokens that an owner allowed to a spender', () => {});
   it('should decrease the amount of tokens that an owner allowed to a spender', () => {});
   it('should return the total number of tokens in existence', () => {});
-  it('should transfer token for a specified address', () => {});
+
+  it('should transfer token for a specified address', async () => {
+    const valueToTransfer = 100 * 10 ** 18;
+
+    await instance.transfer(secondAccount, valueToTransfer);
+
+    const balanceToCheck = await instance.balanceOf(secondAccount);
+
+    assert.equal(balanceToCheck.toNumber(), valueToTransfer);
+  });
+
   it('should get the balance of the specified address', () => {});
   it('should reject if the initial supply equal to 0', () => {});
   it('should reject if a value that a sender want to burn is higher than his balance', () => {});
